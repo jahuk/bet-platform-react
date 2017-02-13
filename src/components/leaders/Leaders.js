d703d3303e54580bf31edf45e040ea16ce890a40
Jan Knigawka <jan.knigawka@gmail.com>
2017-02-13 23:19:30 +0100

    update mock data

--- src/components/leaders/Leaders.js
+++ src/components/leaders/Leaders.js
@@ -3,7 +3,38 @@ import React from 'react';
 const Leaders = () => {
   return (
     <div>
-      Leaders
+      <h2>Leaders:</h2>
+      <table className="table users">
+        <thead>
+        <tr>
+          <th></th>
+          <th>Name</th>
+          <th>Points</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+          <td>1</td>
+          <td><a className="user">Jan</a></td>
+          <td><span className="badge">5</span></td>
+        </tr>
+        <tr>
+          <td>2</td>
+          <td><a className="user">Zbigniew</a></td>
+          <td><span className="badge">4</span></td>
+        </tr>
+        <tr>
+          <td>3</td>
+          <td><a className="user">Krzychu</a></td>
+          <td><span className="badge">2</span></td>
+        </tr>
+        <tr>
+          <td>3</td>
+          <td><a className="user">Wiesław</a></td>
+          <td><span className="badge">2</span></td>
+        </tr>
+        </tbody>
+      </table>
     </div>
   );
 };
--- src/components/scores/Scores.js
+++ src/components/scores/Scores.js
@@ -3,7 +3,32 @@ import React from 'react';
 const Scores = () => {
   return (
     <div>
-      Scores
+      <h2>Scores:</h2>
+      <table className="table results">
+        <tbody>
+        <tr>
+          <td>Polska</td>
+          <td>3</td>
+          <td> - </td>
+          <td>0</td>
+          <td>Rumunia</td>
+        </tr>
+        <tr>
+          <td>Polska</td>
+          <td>3</td>
+          <td> - </td>
+          <td>0</td>
+          <td>Rumunia</td>
+        </tr>
+        <tr>
+          <td>Polska</td>
+          <td>3</td>
+          <td> - </td>
+          <td>0</td>
+          <td>Rumunia</td>
+        </tr>
+        </tbody>
+      </table>
     </div>
   );
 };
