49a257b85bea5e9cebb1b5d01394022e6b0492a1
Jan Knigawka <jan.knigawka@gmail.com>
2017-03-20 19:11:01 +0100

    add basic router

--- src/index.js
+++ src/index.js
@@ -2,15 +2,23 @@ import React from 'react';
 import {render} from 'react-dom';
 import {Provider} from 'react-redux';
 import configureStore from './store/configureStore';
+import {Router, Route, browserHistory} from 'react-router';
+import {syncHistoryWithStore} from 'react-router-redux';
 
 import App from './components/app/App';
 import './scss/styles.scss';
 
 const store = configureStore();
+const history = syncHistoryWithStore(browserHistory, store);
 
 render(
   <Provider store={store}>
-    <App />
+    <Router history={history}>
+      <Route path="/" component={App}>
+        <Route path="/match" component={App}/>
+        <Route path="/user" component={App}/>
+      </Route>
+    </Router>
   </Provider>,
   document.getElementById('app')
 );
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 // Set up your root reducer here...
 import {combineReducers} from 'redux';
+import {routerReducer} from 'react-router-redux';
 
 import scores from './results.reducer';
 
 const rootReducer = combineReducers({
-  scores
+  scores,
+  routing: routerReducer
 });
 
 export default rootReducer;
