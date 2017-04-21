87d834baeb0196bacd194f0d93d415cb47c620f6
Jan Knigawka <jan.knigawka@gmail.com>
2017-04-21 19:40:34 +0200

    add propTypes to Main component

--- src/commons/main/Main.js
+++ src/commons/main/Main.js
@@ -2,11 +2,15 @@ import React from 'react';
 
 const Main = ({children}) => (
   <main>
-    <h1></h1>
+    <h1>React Bet App</h1>
     <section>
       {children}
     </section>
   </main>
 );
+
+Main.propTypes = {
+  children: React.PropTypes.object
+};
 
 export default Main;
