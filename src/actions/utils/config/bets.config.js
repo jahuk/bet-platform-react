fd2bc79413e237dac10687e030c0515c6da207aa
Jan Knigawka <jan.knigawka@gmail.com>
2018-06-13 21:08:41 +0200

    update config and results

--- src/actions/utils/config/bets.config.js
+++ src/actions/utils/config/bets.config.js
@@ -17,8 +17,9 @@ export default {
       'wojtekkuc',
       'mateuszo',
       'adamw',
-      'kamilj',
-      'krzysztofg'
+      'krzysztofg',
+      'sandrak',
+      'bartekb'
     ]
   },
   SETTINGS: {
