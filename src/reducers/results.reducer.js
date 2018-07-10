8f8bbe676c63d4fb792eb92bb1885e33a3ef0d37
Jan Knigawka <jan.knigawka@gmail.com>
2018-07-10 19:26:13 +0200

    semi

--- src/reducers/results.reducer.js
+++ src/reducers/results.reducer.js
@@ -7,7 +7,7 @@ export default (state = defaultState, action) => {
   switch (action.type) {
 
     case AppActions.DATA_FETCH_DONE:
-      return action.results.filter(match => match.matchday === 4 || match.matchday === 5);
+      return action.results.filter(match => match.matchday === 4 || match.matchday === 5 || match.matchday === 6);
       // return action.results.filter(match => match.matchday > 3 && (match.status === 'TIMED' || match.status === 'IN_PLAY' || match.status === 'FINISHED'));
 
     default:
